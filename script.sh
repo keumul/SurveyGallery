e29f13f0db224e7e28b11b4ca71dafe3ebe73b52
RudakovskyS <rudakovsky_sd@mail.ru>
2024-08-12 23:44:13 +0300

    update scripts

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 
 echo "Begin automated deployment..."
 
-echo "DATABASE_URL=postgresql://root:meow@dev-db:5432/nest?schema=public" >>  ./backend/.env
+echo "DATABASE_URL=postgresql://root:meow@survey-gallery-db:5432/nest?schema=public" >>  ./backend/.env
+echo "JWT_SECRET=jwtsecret" >>  ./backend/.env
 
 docker build backend -t survey-gallery-backend
 

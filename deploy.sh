1b304803b9211e54348896a6d9ed9a646009bf7a
RudakovskyS <rudakovsky_sd@mail.ru>
2024-08-22 21:39:50 +0300

    update scripts

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ echo "Begin automated deployment..."
 
 echo "DATABASE_URL=postgresql://root:meow@survey-gallery-db:5432/nest?schema=public" >>  ./backend/.env
 echo "JWT_SECRET=jwtsecret" >>  ./backend/.env
+echo "REACT_APP_API_HOST=http://77.221.139.160:5000" >>  ./backend/.env
 
 docker build backend -t survey-gallery-backend
 
--- prepare_repo.sh
+++ prepare_repo.sh
@@ -4,5 +4,7 @@ git clone https://github.com/keumul/SurveyGallery.git
 cd SurveyGallery || exit 
 git checkout feature/script_for_automatic_deployment
 
-sudo ./infrastructure-setup.sh
+sudo chmod +x infrastructure-setup.sh
 sudo chmod +x deploy.sh
+sudo ./infrastructure-setup.sh
+sudo ./deploy.sh 

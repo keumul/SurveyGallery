339233c90fdafeecb335b59d0e5b899bf60988c2
RudakovskyS <rudakovsky_sd@mail.ru>
2024-08-23 21:13:07 +0300

    check with commented out env var

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ echo "Begin automated deployment..."
 echo "DATABASE_URL=postgresql://root:meow@survey-gallery-db:5432/nest?schema=public" >>  ./backend/.env
 echo "JWT_SECRET=jwtsecret" >>  ./backend/.env
 
+echo "REACT_APP_API_HOST=http://77.221.139.160:5000" >>  ./frontend/public/.env
+
 docker build backend -t survey-gallery-backend
 
 docker build frontend -t survey-gallery-frontend
